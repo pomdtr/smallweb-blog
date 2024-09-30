33f35fa3c143b41281ae98b5de2791e38d78366f
pomdtr <achille.lacoin@gmail.com>
2024-09-30 20:04:37 +0200

    0.14.0

--- main.ts
+++
@@ -1,7 +0,0 @@
-import { serveDir } from "jsr:@std/http/file-server";
-
-export default {
-    fetch: (req) => {
-        return serveDir(req, { fsRoot: "_site" });
-    },
-} satisfies Deno.ServeDefaultExport;
